6bf03958f72b5e35dcecc67cd12652d78a28aa89
ksnksatwik <69215736+ksnksatwik@users.noreply.github.com>
2023-08-02 04:40:11 +0530

    anonouus meathods
    how to wrte ano mwathods

---
+++ Anom.java
@@ -0,0 +1,13 @@
+/**
+ * Anom
+ */
+public class Anom {
+
+    public static void main(String[] args) {
+        Bmw obj;
+        obj = new Bmw();
+        System.out.println("Car object created");
+        obj.start();
+        obj.rbreak();
+    }
+}
